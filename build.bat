424e5a37d8cd68bb47fd8cf5dd4e2c58cfb08048
Joshua Robertson <joshrobertson774@gmail.com>
2023-02-12 21:37:57 +0000

    Update nksdk.

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ goto end
 echo ----------------------------------------
 
 set defs=-D BUILD_NATIVE -D _CRT_SECURE_NO_WARNINGS
-set idir=-I ../../depends/sdl/win32/include -I ../../depends/sdl_mixer/win32/include -I ../../depends/freetype/win32/include -I ../../depends/nksdk -I ../../depends/glew/include -I ../../depends/glew/source -I ../../depends/stb
+set idir=-I ../../depends/sdl/win32/include -I ../../depends/sdl_mixer/win32/include -I ../../depends/freetype/win32/include -I ../../depends/nksdk/nklibs -I ../../depends/glew/include -I ../../depends/glew/source -I ../../depends/stb
 set ldir=-libpath:../../depends/sdl/win32/lib -libpath:../../depends/sdl_mixer/win32/lib -libpath:../../depends/freetype/win32/lib
 set libs=SDL2main.lib SDL2.lib SDL2_mixer.lib freetype.lib opengl32.lib shell32.lib
 set cflg=-std:c++14 -Zc:__cplusplus -W4 -wd4201 -wd4100 -wd4505
@@ -56,7 +56,7 @@ call depends\emsdk\emsdk activate latest
 call depends\emsdk\emsdk_env.bat
 
 set defs=-D BUILD_WEB
-set idir=-I ../../depends/nksdk -I ../../depends/stb
+set idir=-I ../../depends/nksdk/nklibs -I ../../depends/stb
 set libs=-s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_OGG=1 -s USE_VORBIS=1 -s USE_FREETYPE=1 -s MIN_WEBGL_VERSION=1 -s MAX_WEBGL_VERSION=2 -lidbfs.js
 set cflg=-std=c++14
 set lflg=--preload-file ../../assets -s EXPORTED_FUNCTIONS="['_main', '_main_callback']" -s EXPORTED_RUNTIME_METHODS="['ccall']" -s ALLOW_MEMORY_GROWTH
@@ -84,7 +84,7 @@ echo ----------------------------------------
 if not exist tools mkdir tools
 
 pushd tools
-cl ../source/tools/packer.cpp -I ../depends/nksdk -I ../depends/stb -Fe:packer.exe
+cl ../source/tools/packer.cpp -I ../depends/nksdk/nklibs -I ../depends/stb -Fe:packer.exe
 del *.obj
 popd
 
